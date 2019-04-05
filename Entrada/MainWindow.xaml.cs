a964bfe4d324079df51ea7256ab3714b84ef37c5
Luis <lpompa@hotmail.com>
2019-04-05 12:31:35 -0700

    escucha audio

--- Entrada/MainWindow.xaml.cs
+++ Entrada/MainWindow.xaml.cs
@@ -54,7 +54,41 @@ namespace Entrada
             //Buffer
             waveIn.BufferMilliseconds =
                 250;
-            //
+            //¿Que hacer cuando hay muestras disponibles?
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+
+            waveIn.StartRecording();
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesGrabados = e.BytesRecorded;
+            float acumulador = 0.0f;
+            for(int i=0; i<bytesGrabados; i+=2)
+            {
+                //Transformando 2 bytes separados
+                //en una muestra de 16 bits
+                //1.- Toma el segundo byte y el antepone
+                //     8 0's al principio
+                //2.- Hace un OR con el primer byte, al cual
+                //   automaticamente se le llenan 8 0's al final
+                short muestra =
+                    (short)(buffer[i + 1] << 8 | buffer[i]);
+                float muestra32bits =
+                    (float)muestra / 32768.0f;
+                acumulador += Math.Abs(muestra32bits);
+
+            }
+            float promedio = acumulador / 
+                (bytesGrabados / 2.0f);
+            sldMicrofono.Value = (double)promedio;
+
+        }
+
+        private void btnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            waveIn.StopRecording();
         }
     }
 }
